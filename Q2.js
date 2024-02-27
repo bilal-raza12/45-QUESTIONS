19afec05a41acc3fc8aeeb003e2f312676f9db35
bilal-raza12 <160725888+bilal-raza12@users.noreply.github.com>
2024-02-28 01:33:37 +0500

    Add files via upload
    this is the question 2

---
+++ Q2.js
@@ -0,0 +1,2 @@
+var person_name = "Saad";
+console.log("Hello", person_name, ",Would you like to learn some python today?");
---
+++ Q2.ts
@@ -0,0 +1,2 @@
+let person_name:string = "Saad";
+console.log("Hello",person_name,",Would you like to learn some python today?");
